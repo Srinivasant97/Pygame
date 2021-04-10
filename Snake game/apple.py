a6140226846381a7d89f027606dbfb6ae910d7d7
Srinivasant97 <81143895+Srinivasant97@users.noreply.github.com>
2021-04-10 19:59:05 +0530

    Update apple.py

--- Snake game/apple.py
+++ Snake game/apple.py
@@ -16,18 +16,3 @@ class Apple():
 
 
 
-
-
-# import random
-# from config import Config
-
-# class Apple():
-#     def __init__(self):
-#         self.setNewLocation()
-#
-#     def setNewLocation(self):
-#         self.x = random.randint(0, Config.CELLWIDTH - 1)
-#         self.y = random.randint(0, Config.CELLHEIGHT - 1)
-
-
-
